613e334e8666c636686773f6637ecb7bbaba010d
linqitai <514917919@qq.com>
2019-01-17 18:00:37 +0800

    lqt

---
+++ src/components/api/index.js
@@ -0,0 +1,11 @@
+const host = 'http://www.dtc233.com/api/admin/'
+// const host = '/jz'
+// export const ERR_OK = 200
+
+// 登录接口
+// export const loginUrl = host + 'admin/login';
+
+export default{
+  getLoginUrl: host + 'admin/login',
+  ERR_OK: 200
+}
